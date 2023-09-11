ef8b83acd30ca71bfec26b8aaa071b902d6c636a
jvg87 <jvg87.dev@gmail.com>
2023-09-11 18:19:58 -0300

    feat: add card transaction in home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -4,13 +4,31 @@ import Header from '../../components/Header';
 import { AuthContext } from '../../contexts/auth';
 import * as S from './styles';
 
+const greetingMessage = () => {
+  const time = new Date().getHours();
+  let greeting;
+  if (time <= 12) {
+    greeting = 'Bom dia';
+  } else if (time <= 19) {
+    greeting = 'Boa tarde';
+  } else {
+    greeting = 'Boa noite';
+  }
+  return greeting;
+};
+
 export default function Home() {
-  const { showValue, setShowValue } = useContext(AuthContext);
+  const { showValue, setShowValue, user } = useContext(AuthContext);
+
+  const greetingHours = greetingMessage();
+
   return (
     <S.Container>
       <Header title="Resumo" />
       <S.HeaderHome>
-        <S.Message>Bem vindo, John</S.Message>
+        <S.Message numberOfLines={1}>
+          {greetingHours}, {user.name}
+        </S.Message>
         <S.BtnEye onPress={() => setShowValue(!showValue)}>
           {showValue ? (
             <Icon name="eye-off" size={22} color="#fcfcfc" />
@@ -19,14 +37,76 @@ export default function Home() {
           )}
         </S.BtnEye>
       </S.HeaderHome>
-      <S.CardBalance>
-        <S.Text>Seu saldo é de:</S.Text>
+
+      <S.CardBalance style={{ elevation: 5 }}>
+        <S.CardHeader>
+          <S.Text>Seu saldo é de:</S.Text>
+          <S.BtnEdit>
+            <Icon name="edit" size={22} color="#121212" />
+          </S.BtnEdit>
+        </S.CardHeader>
+        <S.Line />
         {showValue ? (
-          <S.Balance animation="bounceIn">R$ 1000</S.Balance>
+          <S.Balance animation="bounceIn">
+            R${' '}
+            {user.balance.toLocaleString('pt-BR', {
+              minimumFractionDigits: 2,
+              maximumFractionDigits: 2,
+            })}
+          </S.Balance>
         ) : (
           <S.Skeleton animation="bounceIn" />
         )}
       </S.CardBalance>
+
+      <S.CardTransaction style={{ elevation: 5 }}>
+        <S.HeaderTransaction>
+          <S.BtnPeriod>
+            <S.Period>Set. 2023</S.Period>
+          </S.BtnPeriod>
+          <S.BtnEdit>
+            <Icon name="edit" size={22} color="#121212" />
+          </S.BtnEdit>
+        </S.HeaderTransaction>
+
+        <S.Line />
+
+        <S.ContainerItem>
+          <S.Item>
+            <S.ContainerTitle>
+              <Icon name="arrow-up-circle" size={22} color="#40916c" />
+              <S.Title>Renda</S.Title>
+            </S.ContainerTitle>
+            {showValue ? (
+              <S.BalanceRevenue animation="bounceIn">R$ 50</S.BalanceRevenue>
+            ) : (
+              <S.SkeletonTransaction animation="bounceIn" />
+            )}
+          </S.Item>
+          <S.Item>
+            <S.ContainerTitle>
+              <Icon name="arrow-down-circle" size={22} color="#e74c3c" />
+              <S.Title>Despesa</S.Title>
+            </S.ContainerTitle>
+            {showValue ? (
+              <S.BalanceExpense animation="bounceIn">R$ 25</S.BalanceExpense>
+            ) : (
+              <S.SkeletonTransaction animation="bounceIn" />
+            )}
+          </S.Item>
+          <S.Item>
+            <S.ContainerTitle>
+              <Icon name="arrow-right-circle" size={22} />
+              <S.Title>Balanço</S.Title>
+            </S.ContainerTitle>
+            {showValue ? (
+              <S.BalanceTotal animation="bounceIn">R$ 20</S.BalanceTotal>
+            ) : (
+              <S.SkeletonTransaction animation="bounceIn" />
+            )}
+          </S.Item>
+        </S.ContainerItem>
+      </S.CardTransaction>
     </S.Container>
   );
 }
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -13,7 +13,7 @@ export const HeaderHome = styled.View`
   flex-direction: row;
   align-items: center;
   justify-content: space-between;
-  border-radius: 0px 0px 10px 10px;
+  border-radius: 0px 0px 25px 25px;
 `;
 
 export const Message = styled.Text`
@@ -24,18 +24,23 @@ export const Message = styled.Text`
 export const BtnEye = styled.TouchableOpacity``;
 
 export const CardBalance = styled.View`
-  justify-content: center;
   align-items: center;
-  padding: 18px;
   height: 120px;
   background-color: #fcfcfc;
-  /* background-color: red; */
   margin-top: -62px;
   margin-left: 24px;
   margin-right: 24px;
-  margin-bottom: 14px;
+  margin-bottom: 76px;
   border-radius: 10px;
-  z-index: 99;
+  z-index: 98;
+`;
+
+export const CardHeader = styled.View`
+  position: relative;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
 `;
 
 export const Text = styled.Text`
@@ -43,8 +48,21 @@ export const Text = styled.Text`
   color: #081c15;
   margin: 10px 0;
 `;
+
+export const BtnEdit = styled.TouchableOpacity`
+  position: absolute;
+  right: 24px;
+`;
+
+export const Line = styled.View`
+  width: 100%;
+  height: 1px;
+  background-color: #ccc;
+  margin-bottom: 18px;
+`;
+
 export const Balance = styled(Animatable.Text)`
-  font-size: 26px;
+  font-size: 22px;
   font-weight: bold;
   color: #081c15;
 `;
@@ -52,6 +70,78 @@ export const Balance = styled(Animatable.Text)`
 export const Skeleton = styled(Animatable.View)`
   margin: 12px 0;
   width: 120px;
+  height: 12px;
+  background-color: #1b4332;
+  border-radius: 8px;
+`;
+
+export const CardTransaction = styled.View`
+  background-color: #fcfcfc;
+  margin-top: -62px;
+  margin-left: 24px;
+  margin-right: 24px;
+  margin-bottom: 14px;
+  border-radius: 10px;
+`;
+
+export const HeaderTransaction = styled.View`
+  position: relative;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+`;
+
+export const BtnPeriod = styled.TouchableOpacity``;
+
+export const Period = styled.Text`
+  font-size: 18px;
+  color: #081c15;
+  margin: 10px 0;
+`;
+
+export const ContainerItem = styled.View`
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+  padding: 18px 0;
+`;
+
+export const Item = styled.View`
+  align-items: center;
+`;
+
+export const ContainerTitle = styled.View`
+  flex-direction: row;
+  gap: 5px;
+`;
+
+export const Title = styled.Text`
+  font-size: 16px;
+  margin-bottom: 5px;
+  color: #081c15;
+`;
+
+export const BalanceRevenue = styled(Animatable.Text)`
+  font-size: 18px;
+  color: #40916c;
+  font-weight: bold;
+`;
+export const BalanceExpense = styled(Animatable.Text)`
+  font-size: 18px;
+  font-weight: bold;
+  color: #e74c3c;
+`;
+
+export const BalanceTotal = styled(Animatable.Text)`
+  font-size: 18px;
+  font-weight: bold;
+`;
+
+export const SkeletonTransaction = styled(Animatable.View)`
+  margin-top: 5px;
+  margin-bottom: 7px;
+  width: 80px;
   height: 12px;
   background-color: #1b4332;
   border-radius: 8px;
